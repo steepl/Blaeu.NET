7cc5f7e9e73d473e9dc26b8ffaf2fccbdaa4c3bd
mickey <50868613+mickeydelsman@users.noreply.github.com>
2022-01-28 17:26:41 +0100

    added Leaflet (CDN) to index.cshtml, added map.css to position map, added map.js to configure map, updated favicon

---
+++ wwwroot/js/map.js
@@ -0,0 +1,10 @@
+﻿/** create new tile (base) layer */
+let openStreetMap = L.tileLayer("https://a.tile.openstreetmap.org/{z}/{x}/{y}.png", { attribution: 'Map data: OpenStreetMap, Map tiles: OpenStreetMap' });
+
+/** create new map and set starting position */
+let map = L.map('map', { layers: [openStreetMap], zoomControl: true })
+    .setView([52.3700000, 4.9000000], 14);
+
+let scaleControl = L.control.scale()
+    .setPosition("bottomright")
+    .addTo(map);
