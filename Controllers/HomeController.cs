539ae72067008073b91317026edd8dc126a0a594
mickey <50868613+mickeydelsman@users.noreply.github.com>
2022-02-23 15:42:51 +0100

    added about page to home controller and removed privacy.cshtml

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace Blaeu.NET.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
